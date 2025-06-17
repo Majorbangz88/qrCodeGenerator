ade9b9967f25f431fd823014c8ba11fbd15f347a
Majorbangz88 <joelchimaobichukwu@gmail.com>
2025-06-17 11:19:54 +0100

    cors

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server running on port ${process.env.PORT ?? 3000}`);
-
-  app.enableCors({
-    origin: [
-      'https://qrcode-frontend-oyyh.onrender.com',
-      'http://localhost:3000'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  });
 }
 bootstrap();
